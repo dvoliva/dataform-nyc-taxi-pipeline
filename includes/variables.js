75056437ef79ed98ad73ca747b41ed7c8ccc1c8a
Diego Venegas <venegasolivad@gmail.com>
2025-01-13 13:47:41 +0000

    corrección tablas BQ

--- includes/variables.js
+++ includes/variables.js
@@ -1,19 +1,17 @@
 
-const ENVIRONMENT = dataform.projectConfig.ENVIRONMENT || "staging";
+const ENVIRONMENT = dataform.projectConfig.vars?.ENVIRONMENT || "staging";
 
 
 const config = {
   staging: {
-    START_DATE: "2016-01-01",
+    START_DATE: "2016-06-01",
     END_DATE: "2016-12-31",
   },
   prod: {
-    START_DATE: "2017-01-01",
+    START_DATE: "2017-08-01",
     END_DATE: "2017-12-31",
   },
 };
 
 
-module.exports = config[dataform.projectConfig.vars.ENVIRONMENT || "staging"];
-
-
+module.exports = config[ENVIRONMENT];
