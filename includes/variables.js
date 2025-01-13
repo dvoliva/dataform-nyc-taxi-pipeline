2c357a6f1801f34e2549e171af565682087b0f8e
Diego Venegas <venegasolivad@gmail.com>
2025-01-13 05:42:55 +0000

    corrigiendo carga de las tablas a BQ

--- includes/variables.js
+++ includes/variables.js
@@ -1,18 +1,19 @@
 
 const ENVIRONMENT = dataform.projectConfig.ENVIRONMENT || "staging";
 
-
 const config = {
   staging: {
     START_DATE: "2016-01-01",
     END_DATE: "2016-12-31",
+    SOURCE_TABLE: "bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2016",
   },
   prod: {
     START_DATE: "2017-01-01",
     END_DATE: "2017-12-31",
+    SOURCE_TABLE: "bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2017",
   },
 };
 
-
 module.exports = config[ENVIRONMENT];
+
 
